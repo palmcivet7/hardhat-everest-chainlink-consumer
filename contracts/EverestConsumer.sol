7674ddc80637854e81989683a7a5c01cdaa4ab93
palmcivet7 <palmcivet@proton.me>
2023-11-04 20:44:33 +0000

    update compiler version

--- contracts/EverestConsumer.sol
+++ contracts/EverestConsumer.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
